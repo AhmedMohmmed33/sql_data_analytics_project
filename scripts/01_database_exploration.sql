ebea82f2ceefdfb0dd363274fdcab332a563dcda
Ahmed Mohamed Elsayed <ahmedelsayed6542404@gmail.com>
2025-03-16 12:01:13 +0200

    Update 01_database_exploration.sql

--- scripts/01_database_exploration.sql
+++ scripts/01_database_exploration.sql
@@ -16,10 +16,10 @@ Table Used:
 -- Retrive a list of all tables in the database --
 -- ============================================ --
 SELECT
-TABLE_CATALOG,
-TABLE_SCHEMA,
-TABLE_NAME,
-TABLE_TYPE
+    TABLE_CATALOG,
+    TABLE_SCHEMA,
+    TABLE_NAME,
+    TABLE_TYPE
 FROM INFORMATION_SCHEMA.TABLES;
 
 -- ============================================ --
