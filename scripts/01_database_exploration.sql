f300afb3208f26f1ba18b90dcc5ae4e3d8e85897
Ahmed Mohamed Elsayed <ahmedelsayed6542404@gmail.com>
2025-03-16 11:40:19 +0200

    Create 01_database_exploration.sql

---
+++ scripts/01_database_exploration.sql
@@ -0,0 +1,60 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+-- ============================================ --
+-- Retrive a list of all tables in the database --
+-- ============================================ --
+SELECT
+TABLE_CATALOG,
+TABLE_SCHEMA,
+TABLE_NAME,
+TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
+
+-- ============================================ --
+-- Retrive all columns for a specific table:
+-- ============================================ --
+
+---------(dim_customers)-------------
+SELECT
+TABLE_SCHEMA,
+TABLE_NAME,
+COLUMN_NAME,
+DATA_TYPE,
+IS_NULLABLE,
+CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
+
+---------(dim_products)-------------
+SELECT
+TABLE_SCHEMA,
+TABLE_NAME,
+COLUMN_NAME,
+DATA_TYPE,
+IS_NULLABLE,
+CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_products';
+
+---------(fact_sales)-------------
+SELECT
+TABLE_SCHEMA,
+TABLE_NAME,
+COLUMN_NAME,
+DATA_TYPE,
+IS_NULLABLE,
+CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'fact_sales';
